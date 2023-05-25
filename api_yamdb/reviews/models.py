4b603f87d9f214adf4307fa18f41998956a52b79
Andrey Fedin <dronbaron.ua@gmail.com>
2023-05-25 22:33:47 +0300

    доработка моделей, добавление related_name и slug

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+
 
 User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Genre, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -27,17 +39,17 @@ class Review(models.Model):
     text = models.TextField()
     score = models.PositiveSmallIntegerField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    title = models.ForeignKey(Title, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
 
     def __str__(self):
         return f"{self.title}: {self.author}"
 
 class Comment(models.Model):
     text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
-    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
         return f"{self.review}: {self.author}"
