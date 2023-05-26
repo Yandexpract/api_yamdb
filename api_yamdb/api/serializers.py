5b527042e4e8f5e81300a6245b47838755c8d14e
Alex Zub <killdamusic@mail.ru>
2023-05-26 21:37:26 +0300

    add: serializers
    fix: remove unnecessary files

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,6 @@
 from django.core.validators import MaxLengthValidator, RegexValidator
 
+from reviews.models import Category, Review, Comment, Genre, Title
 from rest_framework import serializers
 from users.models import User
 from users.validators import validate_username, validate_email
@@ -39,3 +40,56 @@ class GetTokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'confirmation_code')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name', 'slug') 
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    rating = serializers.SerializerMethodField()
+    genre = GenreSerializer(required=False, many=True)
+    category = CategorySerializer(required=False)
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'rating', 'description', 'genre', 'category')
+        model = Title
+
+    def get_rating(self, instance):
+        return 1.0
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(slug_field='username',
+                                          read_only=True,
+                                          required=False,
+                                          default=serializers.
+                                          CurrentUserDefault())
+    title = serializers.SlugRelatedField(slug_field='id', read_only=True)
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        model = Review
+        read_only_fields = ('id', 'author', 'pub_date')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(slug_field='username',
+                                          read_only=True,
+                                          required=False,
+                                          default=serializers.
+                                          CurrentUserDefault())
+    review = serializers.SlugRelatedField(slug_field='id', read_only=True)
+
+    class Meta:
+        fields = ('id', 'author', 'review', 'text', 'pub_date')
+        model = Comment
+        read_only_fields = ('author',)
